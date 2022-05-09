eb55263612c91587da788c7b92318d1573d8d4d8
Matthew Kight <70612402+mdkdoc15@users.noreply.github.com>
2022-05-09 12:33:37 -0400

    Following tutorial set up

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # pong-unity
+
+- Pong folder contains an attempt at doing pong on my own
+- Pong-tutorial is followed from following tutorial https://www.youtube.com/watch?v=YHSanceczXY
--- pong/Assets/Scripts/PaddleMovement.cs
+++ pong/Assets/Scripts/PaddleMovement.cs
@@ -5,35 +5,58 @@ using UnityEngine;
 public class PaddleMovement : MonoBehaviour
 {
     private Transform transform;
+
+    private BoxCollider2D _boxCollider2D;
+    
     // Start is called before the first frame update
-    [SerializeField] private float movementSpeed = .1f;
+    [SerializeField] private float movementSpeed = 5f;
 
     [SerializeField] private string up = "up";
     [SerializeField] private string down = "down";
+
+    [SerializeField] private LayerMask wallMask;
     void Start()
     {
         transform = GetComponent<Transform>();
+        _boxCollider2D = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        Vector3 updatePos = new Vector3(transform.position.x, transform.position.y,
-            transform.position.z);
-        
-        
+        Vector3 updatePos = new Vector3();
+
+        RaycastHit2D hit = new RaycastHit2D();
         if (Input.GetKey(up))
         {
-
-            updatePos.y += movementSpeed;
+            
+            updatePos.y = movementSpeed;
             
         }
         else if (Input.GetKey(down))
         {
-            updatePos.y -= movementSpeed;
-        }
 
-        transform.position = updatePos;
+            updatePos.y = -movementSpeed;
+        }
+        
+        // Check for hits in y direction
+        hit = Physics2D.BoxCast(_boxCollider2D.bounds.center, _boxCollider2D.size, 0, new Vector2(0, updatePos.y),
+            Mathf.Abs(updatePos.y * Time.deltaTime), wallMask);
+        
+        if (hit.collider == null)
+        {
+            // Move the character y
+            transform.Translate(0, updatePos.y * Time.deltaTime, 0);
+        }
+        else
+        {
+            Debug.Log(hit.collider.name);
+        }
+            
+        
+        
 
     }
+    
+    
 }
